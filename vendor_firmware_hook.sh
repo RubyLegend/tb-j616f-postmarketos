e3648e072537a292bee1744318b53b471dea0678
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2025-07-17 11:17:53 +0300

    Add cleanup hook to unmount vendor partition

--- vendor_firmware_hook.sh
+++ vendor_firmware_hook.sh
@@ -7,3 +7,11 @@ mkdir /vendor
 mount /dev/mapper/vendor_a /vendor
  
 # Done. Now firmware should load successfully.
+
+# Cleanup hook before root switch
+cat << EOF > /hooks-cleanup/vendor_firmware_hook.sh
+#!/bin/sh
+umount /vendor
+EOF
+
+chmod +x /hooks-cleanup/vendor_firmware_hook.sh
